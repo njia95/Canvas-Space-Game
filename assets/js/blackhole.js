c40f225f842ed06174bee7919757d5d15e1da8df
njia95 <nathan.jia@mail.utoronto.ca>
2016-06-01 16:19:34 -0400

    try to swtich between the pages

---
+++ assets/js/blackhole.js
@@ -0,0 +1,14 @@
+// called when page loads and sets up event handlers
+function pageLoad() {
+    document.getElementById("start").onclick = show("start-page", "game-page");
+    document.getElementById("game").onclick = show("game-page", "start-page");
+}
+
+function show(shown, hidden) {
+  document.getElementById(shown).style.display='block';
+  document.getElementById(hidden).style.display='none';
+  return false;
+}
+
+// global code
+window.onload = pageLoad;
