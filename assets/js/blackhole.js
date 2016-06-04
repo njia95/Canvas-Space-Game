8134790be9507470c214eec13ca954abdef37f68
njia95 <nathan.jia@mail.utoronto.ca>
2016-06-03 22:23:35 -0400

    try makeing a box

--- assets/js/blackhole.js
+++ assets/js/blackhole.js
@@ -1,14 +1,14 @@
-// called when page loads and sets up event handlers
-function pageLoad() {
-    document.getElementById("start").onclick = show("start-page", "game-page");
-    document.getElementById("game").onclick = show("game-page", "start-page");
-}
+// // called when page loads and sets up event handlers
+// function pageLoad() {
+//     document.getElementById("start").onclick = show("start-page", "game-page");
+//     document.getElementById("game").onclick = show("game-page", "start-page");
+// }
 
-function show(shown, hidden) {
-  document.getElementById(shown).style.display='block';
-  document.getElementById(hidden).style.display='none';
-  return false;
-}
+// function show(shown, hidden) {
+//   document.getElementById(shown).style.display='block';
+//   document.getElementById(hidden).style.display='none';
+//   return false;
+// }
 
-// global code
-window.onload = pageLoad;
+// // global code
+// window.onload = pageLoad;
