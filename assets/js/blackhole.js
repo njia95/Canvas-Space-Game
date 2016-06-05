a77b7f9e772b41b9b41ec12421676e8c83ff47c1
njia95 <nathan.jia@mail.utoronto.ca>
2016-06-05 12:45:42 -0400

    make switching work by putting js in head instead of a separate file

--- assets/js/blackhole.js
+++ assets/js/blackhole.js
@@ -1,14 +1,17 @@
-// // called when page loads and sets up event handlers
-// function pageLoad() {
-//     document.getElementById("start").onclick = show("start-page", "game-page");
-//     document.getElementById("game").onclick = show("game-page", "start-page");
-// }
+// called when page loads and sets up event handlers
+function pageLoad() {
+    alert("a");
+    document.getElementById("start").onclick = show("start-page", "game-page");
+    alert("b");
+    document.getElementById("finish").onclick = show("game-page", "start-page");
+}
 
-// function show(shown, hidden) {
-//   document.getElementById(shown).style.display='block';
-//   document.getElementById(hidden).style.display='none';
-//   return false;
-// }
 
-// // global code
-// window.onload = pageLoad;
+function show(shown, hidden) {
+    alert("la");
+  document.getElementById(shown).style.display='block';
+  alert("sa");
+  document.getElementById(hidden).style.display='none';
+}
+
+window.onload = pageLoad;
