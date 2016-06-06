f14927178d85b81b13e0255420195ebfcbae6d9b
njia95 <nathan.jia@mail.utoronto.ca>
2016-06-06 02:59:20 -0400

    use shorthand function call inside object literal

--- assets/js/blackhole.js
+++ assets/js/blackhole.js
@@ -15,37 +15,7 @@ function showStart() {
     document.getElementById("game-page").style.display = "none";
 }
 
-var redSprite, blueSprite, yellowSprite;
-var myScore;
-
-function startGame() {
-    myGameArea.start();
-    redSprite = new component(50, 50, 10, 10, 1, 1, "red");
-    blueSprite = new component(50, 50, 10, 110, 1, -1, "blue");
-    yellowSprite = new component(50, 50, 50, 60, -1, 1, "yellow"); 
-    myScore = new component("30px", "Consolas", 280, 40, 0, 0, "black", "text");
-}
-
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 1000;
-        this.canvas.height = 640;
-        this.context = this.canvas.getContext("2d");
-        var gamePageNode = document.getElementById("game-page");
-        // insert canvas as the first child of game page
-        gamePageNode.insertBefore(this.canvas, gamePageNode.childNodes[0]);
-        
-        this.frameNo = 0;
-        // updateGameArea runs every 20th millisecond (50 times per second)
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
-
-class component {
+class Component {
     constructor(width, height, x, y, speedX, speedY, color, type) {
         this.width = width;
         this.height = height;
@@ -58,7 +28,7 @@ class component {
     }
 
     update() {
-        var ctx = myGameArea.context;
+        var ctx = GameArea.context;
         if (this.type == "text") {
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = this.color;
@@ -76,23 +46,54 @@ class component {
     }
     
     check() {
-        var bottom = myGameArea.canvas.height - this.height;
+        var bottom = GameArea.canvas.height - this.height;
         if (this.y > bottom || this.y < 0) {
             this.speedY = 0 - this.speedY;
         }
         
-        var right = myGameArea.canvas.width - this.width;
+        var right = GameArea.canvas.width - this.width;
         if (this.x > right || this.x < 0) {
             this.speedX = 0 - this.speedX;
         }
     }
 }
 
+
+var redSprite, blueSprite, yellowSprite;
+var myScore;
+
+function startGame() {
+    GameArea.start();
+    redSprite = new Component(50, 50, 10, 10, 1, 1, "red");
+    blueSprite = new Component(50, 50, 10, 110, 1, -1, "blue");
+    yellowSprite = new Component(50, 50, 50, 60, -1, 1, "yellow"); 
+    myScore = new Component("30px", "Consolas", 280, 40, 0, 0, "black", "text");
+}
+
+var GameArea = {
+    canvas : document.createElement("canvas"),
+    start() {
+        this.canvas.width = 1000;
+        this.canvas.height = 640;
+        this.context = this.canvas.getContext("2d");
+        let gamePageNode = document.getElementById("game-page");
+        // insert canvas as the first child of game page
+        gamePageNode.insertBefore(this.canvas, gamePageNode.childNodes[0]);
+        
+        this.frameNo = 0;
+        // updateGameArea runs every 20th millisecond (50 times per second)
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
 function updateGameArea() {
-    myGameArea.clear();
-    myGameArea.frameNo += 1;
+    GameArea.clear();
+    GameArea.frameNo += 1;
     
-    myScore.text = "SCORE: " + myGameArea.frameNo;
+    myScore.text = "SCORE: " + GameArea.frameNo;
     myScore.update();
     
     redSprite.newPos();
