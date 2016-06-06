1b8e84ae2a6ec31aa2dddf3303c674655ab02b12
njia95 <nathan.jia@mail.utoronto.ca>
2016-06-06 01:11:36 -0400

    add multiple objects and make them move

--- assets/js/blackhole.js
+++ assets/js/blackhole.js
@@ -15,22 +15,13 @@ function showStart() {
     document.getElementById("game-page").style.display = "none";
 }
 
-var sprite;
+var redSprite, blueSprite, yellowSprite;
 
 function startGame() {
     myGameArea.start();
-    sprite = new component(50, 50, 50, 50, "red");
-}
-
-function component(x, y, width, height, color) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.color = color;
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height, this.color);
+    redSprite = new component(50, 50, 10, 10, "red");
+    blueSprite = new component(50, 50, 10, 110, "blue");
+    yellowSprite = new component(50, 50, 50, 60, "yellow"); 
 }
 
 var myGameArea = {
@@ -42,5 +33,38 @@ var myGameArea = {
         var gamePageNode = document.getElementById("game-page");
         // insert canvas as the first child of game page
         gamePageNode.insertBefore(this.canvas, gamePageNode.childNodes[0]);
+        // updateGameArea runs every 20th millisecond (50 times per second)
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
+}
+
+function component(width, height, x, y, color) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    // move right 1 unit every time we update the canvas
+    redSprite.x += 1;
+    redSprite.update();
+    
+    blueSprite.x += 1;
+    blueSprite.y -= 1;
+    blueSprite.update();
+    
+    yellowSprite.x += 1;
+    yellowSprite.y -= 1;
+    yellowSprite.update();
 }
