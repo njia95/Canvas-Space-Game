3d53fb2ccd75fafb814b8306258a014d9f97918b
Barry Yung <barry.yung@mail.utoronto.ca>
2016-06-07 01:53:56 -0400

    there are 10 shapes in total

--- assets/js/blackhole.js
+++ assets/js/blackhole.js
@@ -43,7 +43,7 @@ function stopCount() {
 
 // main
 class Component {
-    constructor(width, height, x, y, speedX, speedY, color, type) {
+    constructor(width, height, x, y, speedX, speedY, color, type, shape) {
         this.width = width;
         this.height = height;
         this.x = x;
@@ -52,6 +52,7 @@ class Component {
         this.speedY = speedY;
         this.color = color;
         this.type = type;
+        this.shape = shape;
     }
 
     draw() {
@@ -70,27 +71,61 @@ class Component {
             // draw square
             // ctx.fillStyle = this.color;
             // ctx.fillRect(this.x, this.y, this.width, this.height);
-            
+
             // draw circles
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.width, 0, 2*Math.PI);
-            ctx.fillStyle = this.color;
-            ctx.fill();     
+            if (this.shape == "circle") {
+                ctx.beginPath();
+                ctx.arc(this.x, this.y, this.width, 0, 2*Math.PI);
+                ctx.fillStyle = this.color;
+                ctx.fill();
+            } else if (this.shape == "square") {
+                ctx.fillStyle = this.color;
+                ctx.fillRect(this.x, this.y, this.width, this.height);
+            } else if (this.shape == "star") {
+                var rot = Math.PI/2*3;
+                var x = this.x;
+                var y = this.y;
+                var spikes = 5;
+                var step=Math.PI/spikes;
+                var outerRadius = 25;
+                var innerRadius = 10;
+
+                ctx.beginPath();
+                ctx.moveTo(this.x,this.y-outerRadius)
+                for(var i=0;i<spikes;i++){
+                    x=this.x+Math.cos(rot)*outerRadius;
+                    y=this.y+Math.sin(rot)*outerRadius;
+                    ctx.lineTo(x,y)
+                    rot+=step
+
+                    x = this.x + Math.cos(rot)*innerRadius;
+                    y = this.y + Math.sin(rot)*innerRadius;
+                    ctx.lineTo(x,y)
+                    rot+=step
+                }
+                ctx.lineTo(this.x,this.y-outerRadius);
+                ctx.closePath();
+                ctx.lineWidth=5;
+                ctx.strokeStyle=this.color;
+                ctx.stroke();
+                ctx.fillStyle=this.color;
+                ctx.fill();
+            }
         }
     }
-    
+
     newPos() { // change position
         this.x += this.speedX;
         this.y += this.speedY;
         this.check();
     }
-    
+
     check() { // check for boundary conditions
         var right = GameArea.canvas.width - this.width;
         if (this.x > right || this.x < this.width) {
             this.speedX = 0 - this.speedX;
         }
-        
+
         var bottom = GameArea.canvas.height - this.height;
         if (this.y > bottom || this.y < this.height) {
             this.speedY = 0 - this.speedY;
@@ -103,17 +138,99 @@ var myScore, sprites, blackholes;
 
 function startGame() {
     GameArea.start();
- 
-    myScore = new Component("30px", "Consolas", 280, 40, 0, 0, "black", "text");   
-    
+
+    myScore = new Component("30px", "Consolas", 280, 40, 0, 0, "black", "text");
+
     sprites = new Array();
     blackholes = new Array();
-    
+
+    randomWidth = new Array();
+
+    // random generator
+    function randgen(purpose) {
+        var i;
+
+        if (purpose == "shape") {
+            i = Math.floor((Math.random() * 3));
+            if (i == 0) {
+                i = "circle";
+            } else if (i == 1) {
+                i = "square";
+            } else {
+                i = "star";
+            }
+        } else if (purpose == "x") {
+            i = Math.floor((Math.random() * 950));
+        } else if (purpose == "y") {
+            i = Math.floor((Math.random() * 560));
+        } else if (purpose == "speed"){
+            if (Math.random() >= 0.5) {
+                i = 1;
+            } else {
+                i = -1;
+            }
+        }
+        return i;
+    }
+
+    // check if x and y were the same in the previous position
+    function samePos(currPos, allPos) {
+        for (var i = 0; i < 10; i++) {
+            if (allPos[i][0] == currPos[0] && allPos[i][1] == currPos[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    var numSprites = 0;
+    var allPos = new Array();
+    for (var i = 0; i < 10; i++) {
+        var empty = [-1, -1];
+        allPos.push(empty);
+    }
+
+    while (numSprites < 10) {
+        var currShape = randgen("shape");
+        var currX = randgen("x");
+        var currY = randgen("y");
+        var currSpeedX = randgen("speed");
+        var currSpeedY = randgen("speed");
+        var pos = new Array();
+        pos.push(currX, currY);
+        while (samePos(pos, allPos)) {
+            currX = randgen("x");
+            currY = randgen("y");
+        }
+        allPos.push(pos);
+        var currWidth;
+        var currHeight;
+
+        if (currShape == "circle") {
+            currWidth = 25;
+            currHeight = 25;
+        } else {
+            currWidth = 50;
+            currHeight = 50;
+        }
+        sprites.push(new Component(currWidth, currHeight, currX, currY, currSpeedX, currSpeedY, "red", "sprite", currShape));
+        numSprites++;
+    }
+
     // insert objects into the sprites
-    sprites.push(new Component(25, 25, 70, 90, 1, 1, "red"));
-    sprites.push(new Component(25, 25, 80, 110, 1, -1, "blue"));
-    sprites.push(new Component(25, 25, 50, 60, -1, 1, "yellow"));
-    
+    // sprites.push(new Component(25, 25, 70, 90, 1, 1, "red", "sprite", "circle"));
+    // sprites.push(new Component(25, 25, 80, 110, 1, -1, "blue", "sprite", "circle"));
+    // sprites.push(new Component(25, 25, 300, 150, 1, -1, "orange", "sprite", "circle"));
+    // sprites.push(new Component(25, 25, 300, 150, 1, -1, "orange", "sprite", "circle"));
+    // sprites.push(new Component(50, 50, 100, 100, -1, 1, "green", "sprite", "square"));
+    // sprites.push(new Component(50, 50, 200, 50, -1, -1, "black", "sprite", "square"));
+    // sprites.push(new Component(50, 50, 200, 50, -1, -1, "black", "sprite", "square"));
+    // sprites.push(new Component(50, 50, 330, 250, -1, -1, "yellow", "sprite", "star"));
+    // sprites.push(new Component(50, 50, 330, 250, -1, -1, "yellow", "sprite", "star"));
+    // sprites.push(new Component(50, 50, 330, 250, -1, -1, "yellow", "sprite", "star"));
+
+
+
     blackholes.push(new Component(25, 25, 0, 0, 0, 0, "assets/img/black.svg", "image"));
 }
 
@@ -126,7 +243,7 @@ var GameArea = {
         let gamePageNode = document.getElementById("game-page");
         // insert canvas as the first child of game page
         gamePageNode.insertBefore(this.canvas, gamePageNode.childNodes[0]);
-        
+
         this.frameNo = 0;
         // updateGameArea runs every 20th millisecond (50 times per second)
         this.interval = setInterval(updateGameArea, 20);
@@ -139,15 +256,15 @@ var GameArea = {
 function updateGameArea() {
     GameArea.clear();
     GameArea.frameNo += 1;
-    
+
     myScore.text = "SCORE: " + GameArea.frameNo;
     myScore.draw();
-    
+
     for (var i = 0; i < sprites.length; i++) {
         sprites[i].newPos();
         sprites[i].draw();
     }
-    
+
     for (var i = 0; i < blackholes.length; i++) {
         blackholes[i].draw();
     }
