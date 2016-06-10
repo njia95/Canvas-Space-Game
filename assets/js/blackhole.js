4fc91a7d24f85b6c20b8c55a87a56f090609851b
Barry Yung <barry.yung@mail.utoronto.ca>
2016-06-10 02:24:26 -0400

    Added some new shapes

--- assets/js/blackhole.js
+++ assets/js/blackhole.js
@@ -55,7 +55,7 @@ function timedCount() {
     setTimeout(function() { timedCount() }, 1000);
 }
 
-// this class used for creating sprites 
+// this class used for creating sprites
 class Component {
     constructor(width, height, x, y) {
         this.width = width;
@@ -70,7 +70,7 @@ class Blackhole extends Component {
         super(width, height, x, y);
         this.src = src;
     }
-    
+
     draw() {
         var ctx = GameArea.context;
         this.image = new Image();
@@ -80,25 +80,149 @@ class Blackhole extends Component {
 }
 
 class Sprite extends Component {
-    constructor(width, height, x, y, speedX, speedY, color) {
+    constructor(width, height, x, y, speedX, speedY, color, shape) {
         super(width, height, x, y);
         this.speedX = speedX;
         this.speedY = speedY;
         this.color = color;
+        this.shape = shape;
     }
-    
+
     draw() {
         var ctx = GameArea.context;
 
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        if (this.type == "text") { // draw score
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = this.color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else if (this.type == "image") { // draw svg
+            this.image = new Image();
+            this.image.src = this.color;
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+
+        } else {
+            if (this.shape == "circle") {
+                var centerX = this.x;
+                var centerY = this.y;
+                var height = 25;
+                var width = 100;
+                ctx.beginPath();
+                ctx.arc(this.x, this.y, this.width, 0, 2*Math.PI);
+                ctx.fillStyle = this.color;
+                ctx.fill();
+
+                ctx.beginPath();
+                ctx.moveTo(centerX, centerY - height/2); // A1
+
+                ctx.bezierCurveTo(
+                centerX + width/2, centerY - height/2, // C1
+                centerX + width/2, centerY + height/2, // C2
+                centerX, centerY + height/2); // A2
+
+                ctx.bezierCurveTo(
+                centerX - width/2, centerY + height/2, // C3
+                centerX - width/2, centerY - height/2, // C4
+                centerX, centerY - height/2); // A1
+                ctx.stroke();
+
+            } else if (this.shape == "square") {
+                ctx.fillStyle = this.color;
+                ctx.fillRect(this.x, this.y, this.width, this.height);
+            } else if (this.shape == "star") {
+                var rot = Math.PI/2*3;
+                var x = this.x;
+                var y = this.y;
+                var spikes = 5;
+                var step=Math.PI/spikes;
+                var outerRadius = 25;
+                var innerRadius = 10;
+
+                ctx.beginPath();
+                ctx.moveTo(this.x,this.y - outerRadius)
+                for (var i = 0; i < spikes; i++){
+                    x = this.x + Math.cos(rot)*outerRadius;
+                    y = this.y + Math.sin(rot)*outerRadius;
+                    ctx.lineTo(x,y)
+                    rot+=step
+
+                    x = this.x + Math.cos(rot)*innerRadius;
+                    y = this.y + Math.sin(rot)*innerRadius;
+                    ctx.lineTo(x,y)
+                    rot += step
+                }
+                ctx.lineTo(this.x,this.y - outerRadius);
+                ctx.closePath();
+                ctx.lineWidth = 5;
+                ctx.strokeStyle = this.color;
+                ctx.stroke();
+                ctx.fillStyle = this.color;
+                ctx.fill();
+            } else if (this.shape == "spaceShip") {
+                ctx.beginPath();
+                ctx.moveTo(this.x,this.y);
+                ctx.lineTo(this.x + 9, this.y + 17.7);
+                ctx.lineTo(this.x - 9, this.y + 17.7);
+                ctx.lineTo(this.x, this.y);
+                ctx.fillStyle = this.color;
+                ctx.closePath();
+                ctx.stroke();
+
+                ctx.beginPath();
+                ctx.fillRect(this.x-12.5, this.y + 17.7, 25, 37.5);
+                ctx.closePath();
+                ctx.stroke();
+
+                var shipY = this.y - 25;
+                var leftShipX = this.x - 14;
+                var rightShipX = this.x + 14;
+
+                ctx.beginPath();
+                ctx.moveTo(leftShipX, shipY + 65);
+                ctx.lineTo(leftShipX, shipY + 77.5);
+                ctx.lineTo(leftShipX - 12.5, shipY + 77.5);
+                ctx.closePath();
+                ctx.stroke();
+
+                ctx.beginPath();
+                ctx.moveTo(rightShipX, shipY + 65);
+                ctx.lineTo(rightShipX, shipY + 77.5);
+                ctx.lineTo(rightShipX + 12.5, shipY + 77.5);
+                ctx.closePath();
+                ctx.stroke();
+
+            } else if (this.shape == "alien") {
+                ctx.beginPath();
+                ctx.arc(centerX, centerY - 20, 40, 0, 2 * Math.PI,false); // x, y, radius, startAngle, endAngle, antiClockWise
+                ctx.fillStyle = this.color;
+                ctx.fill();
+                ctx.closePath();
+
+                ctx.beginPath();
+                ctx.moveTo(centerX, centerY - height/2); // A1
+
+                ctx.bezierCurveTo(
+                centerX + width/2, centerY - height/2, // C1
+                centerX + width/2, centerY + height/2, // C2
+                centerX, centerY + height/2); // A2
+
+                ctx.bezierCurveTo(
+                centerX - width/2, centerY + height/2, // C3
+                centerX - width/2, centerY - height/2, // C4
+                centerX, centerY - height/2); // A1
+
+                ctx.fillStyle =this.color;
+                ctx.fill();
+                ctx.closePath();
+                ctx.stroke();
+            }
+        }
     }
-    
+
     newPos() { // change position
         for (var i = 0; i < blackholes.length; i++) {
-            if (this.x >= blackholes[i].x - HORIZON_DIST && 
-                this.x <= blackholes[i].x + HORIZON_DIST && 
-                this.y >= blackholes[i].y - HORIZON_DIST && 
+            if (this.x >= blackholes[i].x - HORIZON_DIST &&
+                this.x <= blackholes[i].x + HORIZON_DIST &&
+                this.y >= blackholes[i].y - HORIZON_DIST &&
                 this.y <= blackholes[i].y + HORIZON_DIST) {
                 var dx = blackholes[i].x - this.x;
                 var dy = blackholes[i].y - this.y;
@@ -108,17 +232,17 @@ class Sprite extends Component {
         }
         this.x += this.speedX;
         this.y += this.speedY;
-        
+
         for (var i = 0; i < blackholes.length; i++) {
-            if (this.x >= blackholes[i].x - 10 && 
-                this.x <= blackholes[i].x + 10 && 
-                this.y >= blackholes[i].y - 10 && 
+            if (this.x >= blackholes[i].x - 10 &&
+                this.x <= blackholes[i].x + 10 &&
+                this.y >= blackholes[i].y - 10 &&
                 this.y <= blackholes[i].y + 10) {
                 var idx = sprites.indexOf(this);
                 sprites.splice(idx, 1);
                 score -= 50;
             }
-        }  
+        }
         this.check();
     }
 
@@ -126,21 +250,21 @@ class Sprite extends Component {
         var right = GameArea.canvas.width - this.width;
         var bottom = GameArea.canvas.height - this.height;
 
-        // if (this.shape == "square") {
-        //     if (this.x > right || this.x < this.width - HORIZON_DIST) {
-        //         this.speedX = 0 - this.speedX ;
-        //     }
-        //     if (this.y > bottom || this.y < this.height) {
-        //         this.speedY = 0 - this.speedY;
-        //     }
-         
-        if (this.x > right || this.x < this.width) {
-            this.speedX = 0 - this.speedX;
+        if (this.shape == "square") {
+            if (this.x > right || this.x < this.width - 50) {
+                this.speedX = 0 - this.speedX ;
+            }
+            if (this.y > bottom || this.y < this.height - 50) {
+                this.speedY = 0 - this.speedY;
+            }
+        } else {
+            if (this.x > right || this.x < this.width) {
+                this.speedX = 0 - this.speedX;
+            }
+            if (this.y > bottom || this.y < this.height) {
+                this.speedY = 0 - this.speedY;
+            }
         }
-        if (this.y > bottom || this.y < this.height + HORIZON_DIST) {
-            this.speedY = 0 - this.speedY;
-        }
-        
     }
 }
 
@@ -148,7 +272,7 @@ class Sprite extends Component {
 function startGame() {
     document.getElementById("level").innerHTML = level;
     GameArea.initializeCanvas();
-    
+
     // store all the starting positions of the shapes
     var allPos = new Array();
     for (var i = 0; i < 10; i++) {
@@ -189,11 +313,11 @@ function startGame() {
         var currColour =randgen("colour");
 
         // create the sprite
-        sprites.push(new Sprite(currWidth, currHeight, currX, currY, 
-        currSpeedX, currSpeedY, currColour));
+        sprites.push(new Sprite(currWidth, currHeight, currX, currY,
+        currSpeedX, currSpeedY, currColour, currShape));
 
         numSprites++;
-    }   
+    }
 }
 
 // check if x and y were the same in the previous position
@@ -221,7 +345,7 @@ var GameArea = {
         setInterval(updateGameArea, 20);
         setInterval(generateBlackholes, 1000);
     },
-    
+
     clearCanvas() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
@@ -230,33 +354,33 @@ var GameArea = {
 function updateGameArea() {
     GameArea.clearCanvas();
     GameArea.frameNo += 1;
-    
+
     myScore.innerHTML = score;
 
     for (let i = 0; i < sprites.length; i++) {
         sprites[i].newPos();
         if (sprites[i] != null) {
-           sprites[i].draw(); 
+           sprites[i].draw();
         }
     }
-    
+
     for (let i = 0; i < blackholes.length; i++) {
         blackholes[i].draw();
     }
 }
 
 function generateBlackholes() {
-    if (time == 10 || time == 20 || time == 30 || 
+    if (time == 10 || time == 20 || time == 30 ||
     time == 40 || time == 50 || time == 60) {
-        blackholes.push(new Blackhole(50, 50, randgen("x"), randgen("y"), 
+        blackholes.push(new Blackhole(50, 50, randgen("x"), randgen("y"),
         "assets/img/blue.svg"));
     } else if (time == 45 || time == 30 || time == 15) {
         blackholes.push(new Blackhole(50, 50, randgen("x"), randgen("y"),
         "assets/img/purple.svg"));
     } else if (time == 30) {
-        blackholes.push(new Blackhole(50, 50, randgen("x"), randgen("y"), 
+        blackholes.push(new Blackhole(50, 50, randgen("x"), randgen("y"),
         "assets/img/black.svg"));
-    } 
+    }
 }
 
 // random generator
@@ -264,26 +388,35 @@ function randgen(purpose) {
     var i;
     // for shapes
     if (purpose == "shape") {
-        i = Math.floor((Math.random() * 3));
+        i = Math.floor((Math.random() * 4));
         if (i == 0) {
             i = "circle";
         } else if (i == 1) {
             i = "square";
-        } else {
+        } else if (i == 2){
             i = "star";
+        } else if (i == 3) {
+            i = "spaceShip";
+        } else {
+            i = "alien";
         }
     // for x starting position
     } else if (purpose == "x") {
-        i = Math.floor(Math.random() * (MAXWIDTH - 100 + 1)) + 50;
+        i = Math.floor(Math.random() * (MAXWIDTH - 200 + 1)) + 50;
     // for y starting position
     } else if (purpose == "y") {
-        i = Math.floor(Math.random() * (MAXHEIGHT - 100 + 1)) + 50;
+        i = Math.floor(Math.random() * (MAXHEIGHT - 200 + 1)) + 50;
     // for speed
     } else if (purpose == "speed"){
-        if (Math.random() >= 0.5) {
+        var dice = Math.random();
+        if (dice >= 0.25) {
             i = 1;
-        } else {
+        } else if (dice >= 0.5){
             i = -1;
+        } else if (dice >= 0.75) {
+            i = 2;
+        } else {
+            i = -2;
         }
     // for colour only 5 though
     } else if (purpose == "colour"){
@@ -300,11 +433,11 @@ function randgen(purpose) {
 function removeBlackhole(event) {
     var clickX = event.clientX - 10;
     var clickY = event.clientY - 10;
-    
+
     for (let i = 0; i < blackholes.length; i++) {
-        if (clickX >= blackholes[i].x - CLICK_DIST&& 
-            clickX <= blackholes[i].x + CLICK_DIST && 
-            clickY >= blackholes[i].y - CLICK_DIST && 
+        if (clickX >= blackholes[i].x - CLICK_DIST&&
+            clickX <= blackholes[i].x + CLICK_DIST &&
+            clickY >= blackholes[i].y - CLICK_DIST &&
             clickY <= blackholes[i].y + CLICK_DIST) {
             var removed = blackholes.splice(i, 1); // remove one blackhole
             if ((removed[0].src)[11] == "b") { // blue
@@ -317,44 +450,3 @@ function removeBlackhole(event) {
         }
     }
 }
-
-// else {
-//             if (this.shape == "circle") {
-//                 ctx.beginPath();
-//                 ctx.arc(this.x, this.y, this.width, 0, 2*Math.PI);
-//                 ctx.fillStyle = this.color;
-//                 ctx.fill();
-//             } else if (this.shape == "square") {
-//                 ctx.fillStyle = this.color;
-//                 ctx.fillRect(this.x, this.y, this.width, this.height);
-//             } else if (this.shape == "star") {
-//                 var rot = Math.PI/2*3;
-//                 var x = this.x;
-//                 var y = this.y;
-//                 var spikes = 5;
-//                 var step=Math.PI/spikes;
-//                 var outerRadius = 25;
-//                 var innerRadius = 10;
-
-//                 ctx.beginPath();
-//                 ctx.moveTo(this.x,this.y - outerRadius)
-//                 for (var i = 0; i < spikes; i++){
-//                     x = this.x + Math.cos(rot)*outerRadius;
-//                     y = this.y + Math.sin(rot)*outerRadius;
-//                     ctx.lineTo(x,y)
-//                     rot+=step
-
-//                     x = this.x + Math.cos(rot)*innerRadius;
-//                     y = this.y + Math.sin(rot)*innerRadius;
-//                     ctx.lineTo(x,y)
-//                     rot += step
-//                 }
-//                 ctx.lineTo(this.x,this.y - outerRadius);
-//                 ctx.closePath();
-//                 ctx.lineWidth = 5;
-//                 ctx.strokeStyle = this.color;
-//                 ctx.stroke();
-//                 ctx.fillStyle = this.color;
-//                 ctx.fill();
-//             }
-//         }
